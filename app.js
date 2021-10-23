cc7622e27f567fac6e199168e6d154764acec9e7
Heidy <heidymarg5@gmail.com>
2021-10-23 14:41:47 -0300

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/login',(req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/login.html'))
 });
 
-app.listen(process.env.PORT || 4000, ()=>{
+app.listen(process.env.PORT || 4040, ()=>{
     console.log('Servidor corriendo en el puerto 4000')
 });
 
