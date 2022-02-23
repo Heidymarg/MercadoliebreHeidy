2f877eb01347f586c977725bad577025e118b626
Heidy <heidymarg5@gmail.com>
2022-02-23 10:59:32 -0300

    se ve los link de registro y login

--- app.js
+++ app.js
@@ -9,15 +9,13 @@ app.get('/',(req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 });
 
-app.get('./register',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'./views/register.html'))
-});
+app.get('/registro', (req, res) => {res.sendFile(path.resolve(__dirname,'./views/register.html')); });
 
 app.get('/login',(req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/login.html'))
 });
 
-app.listen(process.env.PORT || 4040, ()=>{
-    console.log('Servidor corriendo en el puerto 4000')
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log('Servidor corriendo en el puerto 3001')
 });
 
